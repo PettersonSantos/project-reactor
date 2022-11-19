8dab453736c533dbeafbaac066ac02c9359fd217
Petterson Santos <petterson.santos@outlook.com.br>
2022-11-19 16:45:36 -0300

    subscribing to mono

--- src/test/java/com/projectreact/test/MonoTest.java
+++ src/test/java/com/projectreact/test/MonoTest.java
@@ -2,12 +2,23 @@ package com.projectreact.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @Slf4j
 public class MonoTest {
 
     @Test
-    public void test() {
-        log.info("Everything working as intended");
+    public void monoSubscriber() {
+        String name = "Petterson Santos";
+        Mono<String> mono = Mono.just(name)
+                .log();
+
+        mono.subscribe();
+        log.info("-------------------------------------");
+        StepVerifier.create(mono)
+                .expectNext(name)
+                .verifyComplete();
+
     }
 }
